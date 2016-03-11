5bac020998ad6226e80894d17b3bafcb06bd058f
danamagpusao <danamagpusao>
2016-03-11 11:17:51 +0800

    added countSubstrPattern()

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 def getHammingDistance(str1, str2):
     #compare size of str1 and str2
-    if(len(str1) == len(str2)):
+    if len(str1) == len(str2):
         c = zip(str1,str2)
         d=0;
         for s1,s2 in c:
@@ -11,5 +11,15 @@ def getHammingDistance(str1, str2):
 
     else:
         print "Error! Strings are not equal!"
-  
+
+def countSubstrPattern(original, pattern):
+    count = 0
+    plen = len(pattern)
+    
+    for i in range(len(original)):
+        if original[i:i+plen] == pattern:
+            count += 1
+    return count
+
+
 
